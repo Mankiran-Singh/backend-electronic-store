20b4bda6fc3fc24dcbc660b5498604f87cfc13ff
Mankiran-Singh <mankiransingh22@gmail.com>
2025-05-03 22:09:36 +0530

    refactor: packages dotenv and sql connection package

--- my_modules/connection.js
+++ my_modules/connection.js
@@ -1,15 +1,21 @@
-const mysql = require('mysql');
+const dotenv=require('dotenv');
 
-const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Mani@123',
-    database: 'react_electronic_store'
-});
+dotenv.config({path:'./config.env'});
+console.log('MYSQL_HOST:', process.env.MYSQL_HOST);  
+const mysql = require('mysql2');
+
+const connectionConfig = {
+    host: process.env.MYSQL_HOST || 'localhost',
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE || 'react_electronic_store',
+    port: process.env.MYSQL_PORT || 3306,
+};
+
+const conn = mysql.createConnection(connectionConfig);
 
 conn.connect(function (err) {
     if (err) throw err;
-
     console.log("Connection Created :)");
 });
 
